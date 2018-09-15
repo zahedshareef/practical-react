1f93b0a8bf1bc196d4c81c1185cd0d94769d333f
Zahed Shareef <realzahed@gmail.com>
2018-09-15 22:03:00 +0300

    React state

---
+++ src/components/Counter.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default class Counter extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			count: props.initialCount
+		};
+
+		this.increment = this.increment.bind(this);
+	}
+
+	increment() {
+		this.setState({
+			count: this.state.count + 1
+		});
+	}
+	// increment = () => {
+	// 	this.setState({
+	// 		count: this.state.count + 1
+	// 	});
+	// };
+
+	decrement = () => {
+		this.setState({
+			count: this.state.count - 1
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<h3>Count: {this.state.count}</h3>
+				<button onClick={this.increment}>Increment</button>
+				<button onClick={this.decrement}>Decrement</button>
+			</div>
+		);
+	}
+}
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../logo.svg';
+import Counter from './Counter';
+
+export default class Header extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<Counter initialCount={1} />
+			</div>
+		);
+	}
+}

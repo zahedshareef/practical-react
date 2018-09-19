b01561cf7981e363477073236d470448d23d257a
Zahed Shareef <realzahed@gmail.com>
2018-09-20 01:00:47 +0300

    5.1 Form select and checkbox

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 export default class MyForm extends React.Component {
     state = {
-        name: ''
+        name: 'zahed',
+        rememberMe: false,
+        title: 'Master'
     };
 
     handleChange = (event) => {
@@ -11,12 +13,43 @@ export default class MyForm extends React.Component {
         });
     };
 
+    handleCheckbox = (event) => {
+        this.setState({
+            rememberMe: event.target.checked
+        });
+    };
+
+    handleSelect = (event) => {
+        this.setState({
+            title: event.target.value
+        });
+    };
+
+    handleSubmit = () => {
+        console.log(this.state);
+    };
+
     render() {
         return (
-            <div>
-                Name:
-                <input value={this.state.name} onChange={this.handleChange} />
-                <p>Hi, {this.state.name}</p>
+            <div className="App">
+                <div>
+                    <select value={this.state.title} onChange={this.handleSelect}>
+                        <option>Mr</option>
+                        <option>Mrs</option>
+                        <option>Ms</option>
+                        <option>Master</option>
+                    </select>
+                    <input value={this.state.name} onChange={this.handleChange} />
+                </div>
+                Remember: <input type="checkbox" value={this.state.rememberMe} onChange={this.handleCheckbox} />
+                <div>
+                    <p>
+                        Welcome, {this.state.title}. {this.state.name}. Your preference to remember has been set to:{' '}
+                        {this.state.rememberMe ? 'yes' : 'no'}
+                    </p>
+                </div>
+                <button onClick={this.handleSubmit}>Submit</button>
+                <p>{JSON.stringify(this.state)}</p>
             </div>
         );
     }
